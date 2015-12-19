0eb272dd06da0c15bdd4ecbbf3002b9b601a41d9
Andy Hill <andy@andyhill.us>
2015-12-19 13:53:51 -0500

    add raw webpack config

---
+++ webpack/config.js
@@ -0,0 +1,71 @@
+var path = require('path');
+var util = require('util');
+var autoprefixer = require('autoprefixer-core');
+var pkg = require('../package.json');
+
+var loaders = require('./loaders');
+var plugins = require('./plugins');
+
+var DEBUG = process.env.NODE_ENV === 'development';
+var TEST = process.env.NODE_ENV === 'test';
+
+var jsBundle = path.join('js', util.format('[name].%s.js', pkg.version));
+
+var entry = {
+  app: ['./app.js']
+};
+
+if (DEBUG) {
+  entry.app.push(
+    util.format(
+      'webpack-dev-server/client?http://%s:%d',
+      pkg.config.devHost,
+      pkg.config.devPort
+    )
+  );
+  entry.app.push('webpack/hot/dev-server');
+}
+
+var context = path.join(__dirname, '../public')
+var config = {
+  context: context,
+  cache: DEBUG,
+  debug: DEBUG,
+  target: 'web',
+  devtool: DEBUG || TEST ? 'inline-source-map' : false,
+  entry: entry,
+  output: {
+    path: path.resolve(pkg.config.buildDir),
+    publicPath: '/',
+    filename: jsBundle,
+    pathinfo: false
+  },
+  module: {
+    loaders: loaders
+  },
+  postcss: [
+    autoprefixer
+  ],
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx'],
+    alias: {
+      'decorators': path.join(context, 'decorators'),
+      'components': path.join(context, 'components'),
+      'reducers': path.join(context, 'reducers'),
+      'actions': path.join(context, 'actions'),
+      'constants': path.join(context, 'constants'),
+      'utils': path.join(context, 'utils'),
+      'lib': path.join(context, 'lib')
+    }
+  },
+  devServer: {
+    contentBase: path.resolve(pkg.config.buildDir),
+    hot: true,
+    noInfo: false,
+    inline: true,
+    stats: { colors: true }
+  }
+};
+
+module.exports = config;
