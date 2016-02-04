f7f351359ffa5af335f4b9ad4f8c8f352425ec89
Andy Hill <andy@andyhill.us>
2016-02-04 11:09:46 -0500

    items: make some columns nullable

---
+++ database/migrations/2016_02_04_155747_alter_some_columns_nullable_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterSomeColumnsNullableItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('items', function (Blueprint $table) {
+            $table->string('quantity')->nullable()->change();
+            $table->string('measurement')->nullable()->change();
+            $table->text('comment')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+
+    }
+}
