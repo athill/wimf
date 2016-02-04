d99573503d4307df58c26ce7bdb14a9778f03c1f
Andy Hill <andy@andyhill.us>
2016-02-04 13:53:49 -0500

    stub out localStorage framework

--- resources/assets/util/RemoteOperations.js
+++ resources/assets/util/RemoteOperations.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
 
 export const fetch = (url, resolves, reject) => {
-    chain(axios.get(url), resolves, reject);
+    // const promise = axios({
+    //   method: 'get',
+    //   url
+    // });
+    const promise = makePromise('get', url);
+
+    chain(promise, resolves, reject);
 };
+
+
+const makePromise = (method, url, data)  => {
+  if (location.pathname.includes('/api/demo')) {
+    return localStoragePromise(method, url, data);
+  } else {
+    return axios({
+      method,
+      url,
+      data
+    });
+  }
+};
+
 
 export const post = (url, data, resolves, reject)  => {
   const promise = axios({
@@ -23,4 +43,13 @@ const chain = (promise, resolves, reject = response => console.error(response)) 
     resolves.forEach(resolve => promise.then(resolve));
     //// chain catch
     promise.catch(reject);  
+};
+
+
+const localStoragePromise = (method, url, data) => {
+  return new Promise(
+    (resolve, reject) => {
+      console.log('local storage promise');
+    }
+  );
 };
