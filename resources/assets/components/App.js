9cf631946bf0e4eda1b4ec53687899c421957e3e
Andy Hill <andy@andyhill.us>
2016-02-18 18:43:58 -0500

    move BootstrapContainer to common

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-import BootstrapContainer from './Wimf/BootstrapContainer'
+import BootstrapContainer from './common/BootstrapContainer';
 import Wimf from './Wimf/Wimf';
 import WimfNavbar from './Wimf/WimfNavbar';
 
