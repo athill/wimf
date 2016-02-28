9675d5f252d271127f2a9f76133e5e8bc2fb4b7f
Andy Hill <andy@andyhill.us>
2016-02-28 16:58:39 -0500

    fix create changelog migration

--- database/migrations/2016_02_01_210259_create_changelog_table.php
+++ database/migrations/2016_02_01_210259_create_changelog_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateChangelog2Table extends Migration
+class CreateChangelogTable extends Migration
 {
     /**
      * Run the migrations.
