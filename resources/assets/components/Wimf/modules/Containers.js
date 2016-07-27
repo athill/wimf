1006cbca804ed6005658d8136a19d2399a2cb8f4
Andy Hill <andy@andyhill.us>
2016-07-26 20:09:44 -0400

    start working on fomatting containers on container page

--- resources/assets/components/Wimf/modules/Containers.js
+++ resources/assets/components/Wimf/modules/Containers.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Col, Row, Well } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 import { fetchContainers } from '../../../actions/containers';
+import { Icon } from '../../common/common';
+
+const Container = ({editClickHandler = e => e, deleteClickHandler = e => e,  container }) => (
+    <li>
+        <Well className='container'>
+            <Row>
+                <Col xs={12} sm={6} md={5} lg={5} key={container.name} className='name'>Name: { container.name }</Col>
+                <Col xs={12} sm={6} md={5} lg={5} key={container.description} className='description'>Description: { container.desription }</Col>
+                <Col xs={12} sm={12} md={2} lg={2} key='controls' className='controls'>
+                    foo
+                    <Icon icon='edit' title={`Edit ${container.name}`} onClick={e => deleteClickHandler(container)} />
+                    <Icon icon='remove' title={`Delete ${container.name}`} onClick={e => deleteClickHandler(container)} />
+                </Col>
+            </Row>
+        </Well>
+    </li>
+);
+
 
 const mapStateToProps = ({containers}) => {
   return {
@@ -16,14 +35,7 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const Container = ({ container }) => (
-	<li>
-		<div>
-			<div>Name: { container.name }</div>
-			<div>Description: { container.desription }</div>
-		</div>
-	</li>
-);
+
 
 
 class Containers extends React.Component {
