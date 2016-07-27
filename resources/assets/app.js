8ffe6affc517b8ecd2d06f64de667c7ab14388cf
Andy Hill <andy@andyhill.us>
2016-07-27 18:32:58 -0400

    fix routes; move Router in to app.js

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -2,14 +2,42 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { IndexRoute ,Router, Route, hashHistory } from 'react-router';
+
+
 import App from './components/App';
 import configureStore from './store/configureStore';
 
+//// modules
+import Wimf from './components/Wimf/Wimf';
+import Containers from './components/Wimf/modules/Containers';
+
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App /> 
-  </Provider>,
-  document.getElementById('app')
+const Application = () => (
+	<Provider store={store}>
+		<Router history={hashHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={Wimf} />
+				<Route path="/containers" component={Containers}/>			
+			</Route>
+		</Router>
+	</Provider>
 );
+
+
+ReactDOM.render(
+	React.createElement(Application), document.getElementById('app')
+);
+
+
+/*
+  
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Wimf} />
+			<Route path="/containers" component={Containers}/>
+		</Route>
+	</Router>
+  </Provider>
+*/
