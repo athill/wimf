18884e5746dadda5e5b0e033fbf10860e44f8b1e
Andy Hill <andy@andyhill.us>
2016-07-27 18:36:39 -0400

    fix routing

--- resources/assets/components/App.js
+++ resources/assets/components/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router'
-
 import BootstrapContainer from './common/BootstrapContainer';
 import WimfNavbar from './Wimf/WimfNavbar';
 
-//// modules
-import Wimf from './Wimf/Wimf';
-import Containers from './Wimf/modules/Containers';
 
-
-const App = () => (
+const App = ({children}) => (
 	<div>
 		<WimfNavbar />
         <BootstrapContainer>
-		  <Router history={hashHistory}>
-		    <Route path="/" component={Wimf} />
-		    { /* <Route path="/containers" component={Containers}/> */ }
-		  </Router>
+        {children}
 		</BootstrapContainer> 
 	</div>
 );
