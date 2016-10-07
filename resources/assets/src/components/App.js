a016588027d3a160d8f8149d4cb239a1b80550ae
Andy Hill <andy@andyhill.us>
2016-10-06 21:01:27 -0400

    browser rendering content

--- resources/assets/src/components/App.js
+++ resources/assets/src/components/App.js
@@ -4,13 +4,17 @@ import BootstrapContainer from './common/BootstrapContainer';
 import WimfNavbar from './Wimf/WimfNavbar';
 
 
+// const App = ({children}) => (
+// 	<div>
+// 		<WimfNavbar />
+//         <BootstrapContainer>
+//         {children}
+// 		</BootstrapContainer> 
+// 	</div>
+// );
+
 const App = ({children}) => (
-	<div>
-		<WimfNavbar />
-        <BootstrapContainer>
-        {children}
-		</BootstrapContainer> 
-	</div>
+	<div>Up and running</div>
 );
 
 export default App;
