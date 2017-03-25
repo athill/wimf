7580b3ef28c295cdfb053e2cda4dda260bb55a76
Andy Hill <andy@andyhill.us>
2017-03-25 18:35:45 -0400

    container config menu: kinda sorta displaying properly

---
+++ resources/assets/src/components/common/IconMenu.js
@@ -0,0 +1,115 @@
+/**
+ * Props: http://fauntleroy.github.io/react-simple-dropdown/
+ */
+
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import cx from 'classnames';
+
+const menuItemStyle = {
+  margin: 0,
+  padding: '0.3em',
+};
+export const MenuItem = ({ children, onClick }) => (
+  <li onClick={onClick} style={menuItemStyle}>{children}</li>
+);
+
+const parentStyle = {
+  position: 'relative', 
+  display: 'inline-block'
+};
+
+
+
+export default class  Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    }
+
+    this.menuStyle = { 
+      display: this.state.active ? 'block' : 'none', 
+      position: 'absolute', 
+      top: '20px', 
+      right: 0,
+      backgroundColor: '#EEE',
+      border: 'thin solid black',
+      listStyle: 'none',
+      margin: 0,
+      padding: 0      
+    } 
+
+    this._onWindowClick = this._onWindowClick.bind(this);
+    this._onToggleClick = this._onToggleClick.bind(this);
+  }
+
+  componentDidMount () {
+    window.addEventListener( 'click', this._onWindowClick );
+    window.addEventListener( 'touchstart', this._onWindowClick );
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener( 'click', this._onWindowClick );
+    window.removeEventListener( 'touchstart', this._onWindowClick );
+  }
+
+  render () {
+    const { children, className, triggerLabel } = this.props;
+    // create component classes
+    const active = this.isActive();
+
+   this.menuStyle.display = active ? 'block' : 'none';
+
+    return (
+      <div style={parentStyle}>
+        <span ref="trigger" onClick = { event => this._onToggleClick( event ) }>{ triggerLabel }</span>
+        <ul style={this.menuStyle}>
+          { children }
+        </ul>      
+      </div>
+    );
+  }
+
+  isActive () {
+    return ( typeof this.props.active === 'boolean' ) ?
+      this.props.active :
+      this.state.active;
+  }
+
+  hide () {
+    this.setState({
+      active: false
+    });
+    if( this.props.onHide ){
+      this.props.onHide();
+    }
+  }
+
+  show () {
+    this.setState({
+      active: true
+    });
+    if( this.props.onShow ){
+      this.props.onShow();
+    }
+  }
+
+  _onWindowClick ( event ) {
+    const dropdown_element = findDOMNode( this );
+    if( event.target !== dropdown_element && !dropdown_element.contains( event.target ) && this.isActive() ){
+      this.hide();
+    }
+  }
+
+  _onToggleClick ( event ) {
+    event.preventDefault();
+    if( this.isActive() ){
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+}
+
+
--- resources/assets/src/components/pages/Items/ContainerSelector.js
+++ resources/assets/src/components/pages/Items/ContainerSelector.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
+// import Menu from 'react-menu';
 
 import {Icon} from '../../common/common';
+import IconMenu, { MenuItem } from '../../common/IconMenu';
+
+const triggerLabel = <Icon icon="cog" style={{ fontSize: '0.8em' }} />;
+const ConfigMenu = ({ id }) => (
+	<IconMenu triggerLabel={triggerLabel}>
+		<MenuItem onClick={ e => console.log('edited', id)}>Edit</MenuItem>
+		<MenuItem onClick={ e => console.log('deleted', id)}>Delete</MenuItem>
+	</IconMenu>
+);
+
+
+const ContainerTab = ({ container }) => (
+	<NavItem eventKey={container.id} title={ container.description }>
+		<div style={{ whiteSpace: 'nowrap' }}>{  container.name } <ConfigMenu id={container.id} /></div>
+	</NavItem>
+);
 
 const ContainerSelector = ({containers, handleSelect}) => {
 	const selected = containers.selected ? containers.selected.id : null;
@@ -10,7 +27,7 @@ const ContainerSelector = ({containers, handleSelect}) => {
 				{
 					containers.items.map(container => {
 						return (
-							<NavItem eventKey={container.id} key={container.id} >{  container.name }</NavItem>
+							<ContainerTab container={container} key={container.id} />
 						);
 					})
 				}
