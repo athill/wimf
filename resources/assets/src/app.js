8e0d082a1844dfd4c00deecb190c0595bdf90de4
Andy Hill <andy@andyhill.us>
2017-07-23 10:21:19 -0400

    update webpack, babel

--- resources/assets/src/app.js
+++ resources/assets/src/app.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-//// store creation function 
+// //// store creation function 
 import configureStore from './redux/create';
 
 //// components
