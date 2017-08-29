cf8d9d4a74f0ccc0775a41ba1c825655c2329e8b
Andy Hill <andy@andyhill.us>
2017-08-29 17:05:56 -0400

    import.blade.php: Make import look a little better

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -13,10 +13,13 @@
 
 	<form class="form-inline" action="{{ action('ExportImportController@import') }}" method="post"  enctype="multipart/form-data">
   
-	  <div class="form-group">
-	    <label for="importer">File: </label>
-	    <input type="file" class="form-control" id="importer" name="importer">
-	  </div>
+	  {{-- <div class="form-group"> --}}
+	    {{-- <label for="importer">File: </label>
+	    <input type="file" class="form-control" id="importer" name="importer"> --}}
+		<label class="btn btn-default">
+		    Upload File:  <input name="importer" type="file" hidden>
+		</label>	    
+	  {{-- </div> --}}
 	  {{ csrf_field() }}
 	  <button type="submit" class="btn btn-default">Submit</button>
 	</form>
