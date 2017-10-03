36513fd06ea113d3235d01a222338e31c9c5f98d
Andy Hill <andy@andyhill.us>
2017-10-02 20:58:20 -0400

    jest not working yet

---
+++ resources/assets/js/test/components/App.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import renderer from 'react-test-renderer';
+
+// import { App } from '../components/App';
+
+test('Link changes the class when hovered', () => {
+  // console.log(App);
+  // const component = renderer.create(
+  //   <App />
+  // );
+  // let tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+ });
+
