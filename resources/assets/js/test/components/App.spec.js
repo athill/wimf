89a5d411a50ab3adf69a06c406501aa2498e488a
Andy Hill <andy@andyhill.us>
2017-10-03 20:42:20 -0400

    testing seems to be working

--- resources/assets/js/test/components/App.spec.js
+++ resources/assets/js/test/components/App.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
 // import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-// import { App } from '../components/App';
+import App from '../../components/App';
 
 test('Link changes the class when hovered', () => {
-  // console.log(App);
+  const output = shallow(<App />);
+  expect(output.find('Provider').length).toBe(1);
+  expect(output.find('Router').length).toBe(1);
+
   // const component = renderer.create(
   //   <App />
   // );
