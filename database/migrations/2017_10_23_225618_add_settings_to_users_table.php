0a85f310298f5e385cc527ec81c27395bfb9fccd
Andy Hill <andy@andyhill.us>
2017-10-25 20:39:54 -0400

    fix add settings migration

--- database/migrations/2017_10_23_225618_add_settings_to_users_table.php
+++ database/migrations/2017_10_23_225618_add_settings_to_users_table.php
@@ -14,7 +14,7 @@ class AddSettingsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->json('settings')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddSettingsToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('settings')->nullable();
+            $table->dropColumn('settings');
         });
     }
 }
