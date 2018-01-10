d8d1cb81999778e904a03f0c5882ed06de96ef59
Andy Hill <andy@andyhill.us>
2018-01-10 01:12:44 +0000

    fix phpunit container tests

--- tests/Feature/ContainerTest.php
+++ tests/Feature/ContainerTest.php
@@ -39,126 +39,123 @@ class ContainerTest extends TestCase {
      * @return void
      */
     public function testGetContainers() {
-        //// verify json
-        $response = $this->json('get', self::CONTAINERS_PATH)
-		  ->assertJson([
-                'containers' => ['*' => ['id', 'name', 'description']]
+        $this->json('get', self::CONTAINERS_PATH)
+            //// verify json
+		    ->assertJsonStructure([
+                'containers',
                 'selected'
              ]);
         //// verify freezer added to db
-        $response->seeInDatabase('containers', [
+        $this->assertDatabaseHas('containers', [
         	'user_id' => $this->defaultUser->id,
         	'name' => 'Freezer'
         ]);
     }
 
-    // /**
-    //  * Verify getting a container
-    //  *
-    //  * @return void
-    //  */
-    // public function testGetContainer() {
-    //     $container = $this->getFakeContainer();
-    //     $categoryArgs = [
-    //         'container_id' => $container->id
-    //     ];
-    //     $cat1 = $this->getFakeCategory($categoryArgs);
-    //     $cat2 = $this->getFakeCategory($categoryArgs);
-    //     $item1 = $this->getFakeItem($cat1->id);
-    //     $item2 = $this->getFakeItem($cat1->id);
-    //     $item3 = $this->getFakeItem($cat2->id);
-    //     $this->get(self::CONTAINERS_PATH.'/'.$container->id);
-    //     // $json = self::getResponseContentAsJson($this->response);
-    //     $json = $this->getResponseContentAsJson();
-    //     $this->seeJsonStructure([
-    //         'name', 
-    //         'id', 
-    //         'categories' => [
-    //             '*' => [
-    //                 'name',
-    //                 'items' => [
-    //                     '*' => [
-    //                         'id', 'name', 'quantity', 'comment', 'category_id', 'date', 'category'
-    //                     ]
-    //                 ]
-    //             ]
-    //         ]
+    /**
+     * Verify getting a container
+     *
+     * @return void
+     */
+    public function testGetContainer() {
+        $container = $this->getFakeContainer();
+        $categoryArgs = [
+            'container_id' => $container->id
+        ];
+        $cat1 = $this->getFakeCategory($categoryArgs);
+        $cat2 = $this->getFakeCategory($categoryArgs);
+        $item1 = $this->getFakeItem($cat1->id);
+        $item2 = $this->getFakeItem($cat1->id);
+        $item3 = $this->getFakeItem($cat2->id);
+        $response = $this->get(self::CONTAINERS_PATH.'/'.$container->id);
+        $response->assertJsonStructure([
+            'name', 
+            'id', 
+            'categories' => [
+                '*' => [
+                    'name',
+                    'items' => [
+                        '*' => [
+                            'id', 'name', 'quantity', 'comment', 'category_id', 'date', 'category'
+                        ]
+                    ]
+                ]
+            ]
             
-    //     ]);
-    // }
+        ]);
+    }
 
-    // public function testPostContainer() {
-    //     $this->post(self::CONTAINERS_PATH, $this->defaultParams);
-    //     $json = $this->getResponseContentAsJson();
-    //     $this->seeJson(self::mapSeeJson($this->defaultParams));
-    //     $this->seeInDatabase('containers', $this->defaultParams);
+    public function testPostContainer() {
+        $response = $this->json('POST', self::CONTAINERS_PATH, $this->defaultParams);
+        // $json = $this->getResponseContentAsJson();
+        $response->assertJsonStructure(['name', 'description']);
+        $this->assertDatabaseHas('containers', $this->defaultParams);
 
-    // }
+    }
 
-    // public function testPutContainer() {
-    //     $this->post(self::CONTAINERS_PATH, $this->defaultParams);
-    //     $json = $this->getResponseContentAsJson();
-    //     $newName = $this->faker->word;
-    //     $updateParams = [
-    //         'name' => $newName
-    //     ];
-    //     $this->putContainer($json['id'], $updateParams);
-    //     $this->seeJson(self::mapSeeJson($updateParams));
+    public function testPutContainer() {
+        $response = $this->post(self::CONTAINERS_PATH, $this->defaultParams);
+        $json = $this->getResponseContentAsJson($response);
+        $newName = $this->faker->word;
+        $updateParams = [
+            'name' => $newName
+        ];
+        $this->putContainer($json['id'], $updateParams)
+            ->assertJson($updateParams);
 
-    //     $this->seeInDatabase('containers', [
-    //         'id' => $json['id'],
-    //         'name' => $newName,
-    //         'user_id' => $this->defaultUser->id
-    //     ]);
-    // } 
+        $this->assertDatabaseHas('containers', [
+            'id' => $json['id'],
+            'name' => $newName,
+            'user_id' => $this->defaultUser->id
+        ]);
+    } 
 
-    // public function testPutInvalidContainerId() {
-    //     $id = $this->faker->word;
-    //     $this->putContainer($id, []);
-    //     $this->seeJson(['_error' => 'Invalid id: '.$id]);
-    // } 
+    public function testPutInvalidContainerId() {
+        $id = $this->faker->word;
+        $this->putContainer($id, [])
+            ->assertJson(['_error' => 'Invalid id: '.$id]);
+    } 
 
-    // public function testPutContainerDuplicateName() {
-    //     $container1 = $this->getFakeContainer();
-    //     $container2 = $this->getFakeContainer();
-    //     $this->putContainer($container1->id, ['name' => $container2->name])
-    //         ->seeJsonStructure(['_error']);
-    // }
+    public function testPutContainerDuplicateName() {
+        $container1 = $this->getFakeContainer();
+        $container2 = $this->getFakeContainer();
+        $this->putContainer($container1->id, ['name' => $container2->name])
+            ->assertJsonStructure(['_error']);
+    }
 
-    // public function testDeleteContainer() {
-    //     $container = $this->getFakeContainer();
-    //     $criteria = [
-    //         'name' => $container->name,
-    //         'user_id' => $this->defaultUser->id
-    //     ];
+    public function testDeleteContainer() {
+        $container = $this->getFakeContainer();
+        $criteria = [
+            'name' => $container->name,
+            'user_id' => $this->defaultUser->id
+        ];
 
-    //     $this->seeInDatabase('containers', $criteria);        
-    //     $this->delete(self::CONTAINERS_PATH.'/'.$container->id);        
-    //     $this->notSeeInDatabase('containers', $criteria);
-    // }
+        $this->assertDatabaseHas('containers', $criteria);        
+        $this->delete(self::CONTAINERS_PATH.'/'.$container->id);        
+        $this->assertDatabaseMissing('containers', $criteria);
+    }
 
-    // public function testDeleteNonExistingContainer() {
-    //     $this->delete(self::CONTAINERS_PATH.'/'.$this->faker->word);
-    //     $this->assertEquals('', $this->response->getContent());
-    // }
+    public function testDeleteNonExistingContainer() {
+        $response = $this->delete(self::CONTAINERS_PATH.'/'.$this->faker->word);
+        $this->assertEquals('', $response->getContent());
+    }
 
-    // //// model tests
-    // public function testNameExistsIsTrueIfNameExists() {
-    //     $container = $this->getFakeContainer();
-    //     $this->assertTrue(Container::nameExists($container->name));
+    //// model tests
+    public function testNameExistsIsTrueIfNameExists() {
+        $container = $this->getFakeContainer();
+        $this->assertTrue(Container::nameExists($container->name));
+    }
 
-    // }
+    public function testNameExistsIsFalseIfNameDoesNotExists() {
+        $container = $this->getFakeContainer();
+        $name = $this->faker->word;
+        $this->assertNotEquals($name, $container->name);
+        //// TODO: verify and delete if name exists
+        $this->assertFalse(Container::nameExists($name));
+    }
 
-    // public function testNameExistsIsFalseIfNameDoesNotExists() {
-    //     $container = $this->getFakeContainer();
-    //     $name = $this->faker->word;
-    //     $this->assertNotEquals($name, $container->name);
-    //     //// TODO: verify and delete if name exists
-    //     $this->assertFalse(Container::nameExists($name));
-    // }
-
-    // private function putContainer($id, $args) {
-    //     return $this->put(self::CONTAINERS_PATH.'/'.$id, $args);
-    // }
+    private function putContainer($id, $args) {
+        return $this->json('PUT', self::CONTAINERS_PATH.'/'.$id, $args);
+    }
 
 }
