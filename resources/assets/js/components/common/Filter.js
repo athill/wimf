b82e7451de2747778e51669c7ba75ead2dc6a597
Andy Hill <andy@andyhill.us>
2018-06-10 18:02:45 -0400

    filter: add clear button, autofocus

--- resources/assets/js/components/common/Filter.js
+++ resources/assets/js/components/common/Filter.js
@@ -1,21 +1,72 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
+import { Button, ControlLabel, Form, FormControl, FormGroup, InputGroup } from 'react-bootstrap';
 
-const Filter = ({ handleChange }) => (
-	<form className="form-horizontal" onSubmit={ e => e.preventDefault() }>
-		<FormGroup className="item-filter" controlId="items-filter">
+import { Icon } from './common';
+
+class Filter extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.textInput = React.createRef();
+
+		this._handleChange = this._handleChange.bind(this);
+		this._clear = this._clear.bind(this);
+		this.state = {
+			value: ''
+		}
+	}
+
+	componentDidMount() {
+		this.textInput.current.focus();
+	} 
+
+	_handleChange(e) {
+		const value = e.target.value;
+		this.setState({
+			value
+		})
+		this.props.handleChange(value);
+	}
+
+	_clear() {
+		this.setState({
+			value: ''
+		})
+		this.props.handleChange('');
+		// console.log(this.textInput.current)
+		this.textInput.current.focus();	
+	}
+
+	render() {
+		return (
+			<Form horizontal onSubmit={ e => e.preventDefault() }>
+				<FormGroup className="item-filter" controlId="items-filter">
+					<InputGroup>
+						<InputGroup.Addon>Filter:</InputGroup.Addon>
+						<FormControl value={this.state.value} autoFocus inputRef={ this.textInput }
+							onChange={this._handleChange}  />
+						<InputGroup.Button>
+							<Button onClick={this._clear}>
+								<Icon icon='times-circle' />
+							</Button>
+						</InputGroup.Button>
+					</InputGroup>
+				</FormGroup>
+			</Form>
+		);
+	}
+}
+
+Filter.displayName = 'Filter';
+Filter.propTypes = {
+	handleChange: PropTypes.func
+};
+/*
 				<ControlLabel className="col-xs-2">Filter:</ControlLabel>
 				<div className='col-xs-10'>
 					<FormControl type='text' componentClass="input"
 						onChange={e => handleChange(e.target.value)} />
 				</div>
-			</FormGroup>
-	</form>
-);
-Filter.displayName = 'Filter';
-Filter.propTypes = {
-	handleChange: PropTypes.func
-};
-
+*/
 export default Filter;
