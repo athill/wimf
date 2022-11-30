33088b0b76e97d7d5f1225e5cce79371273038c4
Andy Hill <athill@iu.edu>
2022-11-29 20:28:02 -0500

    Update to laravel 9

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp resources/views/app-template.blade.php public/index.html
-react-scripts build
+react-scripts  --openssl-legacy-provider build
 cp -rf build/* public
 mv public/index.html resources/views/app.blade.php
